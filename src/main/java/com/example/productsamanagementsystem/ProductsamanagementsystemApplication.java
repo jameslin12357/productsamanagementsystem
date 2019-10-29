ceddfcbdf5caa7b008255e70d2381c002aee0f95
jameslin12357 <jameslin12357@qq.com>
2019-10-29 13:50:00 +0800

    cm1

---
+++ src/main/java/com/example/productsamanagementsystem/ProductsamanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.productsamanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsamanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsamanagementsystemApplication.class, args);
+    }
+
+}
